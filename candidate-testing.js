078a0dad25de206ccb4d06b4f8c0ca84096e3a5a
Brett Barnett <brett_barnett@sbcglobal.net>
2024-01-29 17:39:04 -0600

    Completed Part 3 of Candidate Testing

--- candidate-testing.js
+++ candidate-testing.js
@@ -27,7 +27,6 @@ let candidateAnswers = [];
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
   candidateName = input.question("Enter your name: ");
-
 }
 
 function askQuestion() {
